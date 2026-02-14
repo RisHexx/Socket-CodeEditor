0caf67984e1419745103ecfac8dcbe54e30a158c
Rishexx <privaterishabhh@gmail.com>
2026-02-14 20:41:23 +0530

    Added Explanations

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,25 @@ const io = new Server(server, {
 
 const userSocketMap = {};
 
+/* 
+
+{
+  socketid : name
+  "abc123": "Rahul",
+  "xyz789": "Aisha"
+}
+*/
+
+// internal socket rooms
+/*
+rooms = {
+  "room1": Set { "abc123", "xyz789" },
+  "room2": Set { "pqr456" }
+}
+*/
+
 function getAllClients(roomId) {
+
   return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
     (socketId) => {
       return {
@@ -45,25 +63,28 @@ io.on("connection", (socket) => {
   });
 
     socket.on("disconnecting", () => {
-    const rooms = [...socket.rooms];
+    // socket.rooms = It is a Set containing all rooms the socket is currently in.
+    const rooms = [...socket.rooms]; 
     rooms.forEach((roomId) => {
+      // socket.to(roomId) : send message to everyone expect this user -> broadcasting
       socket.to(roomId).emit(Actions.DISCONNECTED, {
         socketId: socket.id,
         username: userSocketMap[socket.id],
       });
     });
     delete userSocketMap[socket.id];
-    socket.leave();
 });
 
 
   socket.on(Actions.CODE_CHANGE, ({ roomId, code }) => {
     // BUG FIX: Use socket.in() instead of io.in() to broadcast to everyone EXCEPT the sender
     // io.in() sends to ALL clients including sender, causing duplicate updates and potential loops
+    // socket.in(roomid) is also send message to everyone expect this user -> broadcasting
     socket.in(roomId).emit(Actions.CODE_CHANGE, { code });
   });
 
   socket.on(Actions.SYNC_CODE, ({ code, socketId }) => {
+    // this to particulare that socketid user
     io.to(socketId).emit(Actions.CODE_CHANGE, { code });
   });
 });
